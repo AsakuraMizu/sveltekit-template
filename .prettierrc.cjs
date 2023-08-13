d2966929d14bad1ef462564ed6f0f7f2915f3263
AsakuraMizu <asakuramizu111@gmail.com>
2023-08-13 17:50:38 +0800

    chore: update deps

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,12 +2,6 @@ module.exports = {
   singleQuote: true,
   printWidth: 100,
   plugins: ['prettier-plugin-svelte', '@trivago/prettier-plugin-sort-imports'],
-  overrides: [
-    {
-      files: '*.svelte',
-      options: { parser: 'svelte' },
-    },
-  ],
   importOrder: ['<THIRD_PARTY_MODULES>', '^\\$(.*)$', '^[./]'],
   htmlWhitespaceSensitivity: 'ignore',
 };
